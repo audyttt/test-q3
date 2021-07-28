86e141ccc7e093ddc8ec57c2c1d701af29cdc1be
audyttt <43567811+audyttt@users.noreply.github.com>
2021-07-28 16:10:47 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const args = process.argv.slice(2)
+if (args[0] === 'FUNDCODE') {
+    console.log('Node is Running')
+}
+else if (args[0] === 'BEQSSF') {
+    console.log('11.247')
+}
+else if (args[0] === 'BM70SSF') {
+    console.log('9.9774')  
+}
+else{
+    console.log('Not found')
+}
